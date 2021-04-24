4d4b7d555497a8431101b840babccbbfdd4c154a
flashback220 <rayhidayat220@gmail.com>
2021-04-24 18:46:06 +0700

    test

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const about = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni, dignissimos assumenda! Vitae, cupiditate maxime! Temporibus numquam consequatur cupiditate nemo velit?</p>
+            <p>Magni distinctio in, nulla debitis saepe sequi exercitationem earum, praesentium, deleniti accusamus mollitia. Quam ab doloribus aut unde vitae quidem.</p>
+        </div>
+    )
+}
+
+export default about
+
+
